4934588d59e0282f27826b8ab12a0e1bae6e23ce
Connor Larson <larsonconnor1@gmail.com>
2026-01-16 00:02:27 -0700

    cleaned TODO

--- src/horus/horus.py
+++ src/horus/horus.py
@@ -13,27 +13,18 @@
 # see if there is a difference between them, if so, alert
 
 #TODO 
-# look in current directory for likely files if targets does not exist
 # clean targets list before placing in list
 # make code comments fancier, better organization
 # warn if an old folder has not been checked 
 # record the precise time in the JSON output
-# check if the command exists first 
-# modulize ts 
 # throw error if the target syntax is invalid
-# maybe switch os to pathlib 
 # the datetime folder creation needs to check if 
 # correct datetime, probably just fix it yourself
-# clean all text at input
-# handle already having a folder for the day
-# check if email has not been changed
-# check if exit() is the right move there
+# clean all text at input+
 # handle no folder for yesterday
 
 #imports
 from __future__ import annotations
-import os.path                     
-              
 
 
 from horus.targets import process_target_list, convert_to_set, jsonl_to_dict
